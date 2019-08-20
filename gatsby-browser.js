1c9c3095df660a47bb15d0264ca92e91c6d700ce
Iz P <pehisaac@gmail.com>
2019-08-19 23:52:00 -0600

    Brought in syntax highlighting.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+// gatsby-browser.js
+require("prismjs/themes/prism-okaidia.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-125028143-6`,
       },
     },
     `gatsby-plugin-feed`,
