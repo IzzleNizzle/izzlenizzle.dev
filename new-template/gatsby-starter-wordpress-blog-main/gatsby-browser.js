817e696fa0086aebcba55e55ec8c503ef9277057
Izzle <pehisaac@gmail.com>
2022-07-26 19:08:45 -0400

    files for new template

---
+++ new-template/gatsby-starter-wordpress-blog-main/gatsby-browser.js
@@ -0,0 +1,12 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+
+// normalize CSS across browsers
+import "./src/css/normalize.css"
+
+// custom CSS styles
+import "./src/css/style.css"
+
+// code block formatting
+import "prismjs/themes/prism-okaidia.css"
